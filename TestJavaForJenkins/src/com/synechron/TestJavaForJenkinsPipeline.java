9ba1fd188e013d3f5420f07392165efb8e838d46
digvan1 <digvan1@host.docker.internal>
2024-12-12 05:42:24 +0000

    jenkins

---
+++ TestJavaForJenkins/src/com/synechron/TestJavaForJenkinsPipeline.java
@@ -0,0 +1,10 @@
+package com.synechron;
+
+public class TestJavaForJenkinsPipeline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("********************* Jenkins started *******************");
+	}
+
+}
